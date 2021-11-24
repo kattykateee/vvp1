194f7960701287a082cf527aa43c4ae1919dfe0a
kattykateee <91073116+kattykateee@users.noreply.github.com>
2021-11-24 23:16:27 +0300

    Add files via upload

---
+++ Lab5/Lab_5_Task_1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	cout << "Введите x1, x2, y1, y2" << endl;
+	double x1, x2, y1, y2;
+	cin >> x1 >> x2 >> y1 >> y2;
+	cout << "s = " << sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)) << endl;
+}
---
+++ Lab5/Lab_5_Task_3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{	
+	setlocale(0, "");
+	cout << "Введите A, B, C" << endl;
+	int A, B, C, pr;
+	cin >> A >> B >> C;
+	pr = (C - A) * (B - C);
+	cout << pr << endl;
+}
